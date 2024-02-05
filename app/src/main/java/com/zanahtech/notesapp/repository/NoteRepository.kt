9fd5fc80c508a9faff91b3cd0a8045348ff345f3
kiprono <im.kiprono@gmail.com>
2024-02-05 09:13:17 +0300

    adapter

---
+++ app/src/main/java/com/zanahtech/notesapp/repository/NoteRepository.kt
@@ -0,0 +1,20 @@
+package com.zanahtech.notesapp.repository
+
+import com.zanahtech.notesapp.database.NoteDatabase
+import com.zanahtech.notesapp.model.Note
+
+class NoteRepository(private val db: NoteDatabase) {
+
+
+    suspend fun insertNote(note: Note) = db.getNoteDao().insertNote(note)
+    suspend fun deletetNote(note: Note) = db.getNoteDao().deleteNote(note)
+    suspend fun updateNote(note: Note) = db.getNoteDao().updateNote(note)
+
+    fun getAllNotes() = db.getNoteDao().getAllNotes()
+    fun searchNote(query: String?) = db.getNoteDao().searchNote(query)
+
+
+
+
+
+}
